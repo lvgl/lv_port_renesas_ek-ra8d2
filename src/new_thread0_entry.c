c051b0b222c335873578bdc9fbf29bd3da2b95ec
Jeremy Baker <jeremy.baker@renesas.com>
2026-01-08 14:37:27 +0000

    Update indev driver

--- src/new_thread0_entry.c
+++ src/new_thread0_entry.c
@@ -44,6 +44,22 @@ void new_thread0_entry(void *pvParameters)
     while (1)
     {
       lv_timer_handler();
+#if (1 == INDEV_EVENT_DRIVEN)
+        BaseType_t status;
+
+        status = xSemaphoreTake( g_irq_binary_semaphore, 0);
+        if(pdTRUE == status)
+        {
+            lv_indev_t * indev_touchpad = NULL;
+            indev_touchpad = lv_indev_get_next(NULL);
+            if(NULL == indev_touchpad)
+            {
+                LV_ASSERT(0);
+            }
+            /* Call this anywhere you want to read the input device */
+            lv_indev_read(indev_touchpad);
+        }
+#endif
       vTaskDelay (1);
     }
 }
